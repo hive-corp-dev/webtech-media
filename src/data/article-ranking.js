5d8a6d16d3ab12071510befa1f6a074c74adb6f5
Tatsuya <a.tatsu0117@gmail.com>
2024-12-28 18:43:39 +0900

    added articles

--- src/data/article-ranking.js
+++ src/data/article-ranking.js
@@ -1,1 +1,5 @@
-export const rankingPostIds = ["astro-get-url.mdx"];
+export const rankingPostIds = [
+  "astro-get-url.mdx",
+  "nextjs-14-app-router-google-analytics.mdx",
+  "nextjs-14-app-router-microsoft-clarity.mdx",
+];
