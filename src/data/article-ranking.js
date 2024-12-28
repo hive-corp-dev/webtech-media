44801c499fcb7240b899a7dbc9f2e7e4387139c7
Tatsuya <a.tatsu0117@gmail.com>
2024-12-29 00:31:20 +0900

    fixed some

--- src/data/article-ranking.js
+++ src/data/article-ranking.js
@@ -2,4 +2,5 @@ export const rankingPostIds = [
   "astro-get-url.mdx",
   "nextjs-14-app-router-google-analytics.mdx",
   "nextjs-14-app-router-microsoft-clarity.mdx",
+  "astro-partytown-google-analytics.mdx",
 ];
