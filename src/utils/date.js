a80beaf98ab6f23cfb828eb2f726b1526b9915f4
Tatsuya <a.tatsu0117@gmail.com>
2025-01-01 23:47:03 +0900

    added article

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,11 +1,25 @@
-import { toZonedTime } from "date-fns-tz";
+// import { toZonedTime } from "date-fns-tz";
+
+// export const formatDate = (dateData) => {
+//   const data = toZonedTime(dateData, "Asia/Tokyo");
+
+//   const year = data.getFullYear();
+//   const month = data.getMonth() + 1;
+//   const date = data.getDate();
+
+//   return `${year}.${month < 10 ? "0" + month : month}.${date < 10 ? "0" + date : date}`;
+// };
+
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.locale("ja");
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const formatDate = (dateData) => {
-  const data = toZonedTime(dateData, "Asia/Tokyo");
+  const localDate = dayjs(dateData).tz("Asia/Tokyo", true);
 
-  const year = data.getFullYear();
-  const month = data.getMonth() + 1;
-  const date = data.getDate();
-
-  return `${year}.${month < 10 ? "0" + month : month}.${date < 10 ? "0" + date : date}`;
+  return localDate.format("YYYY.MM.DD");
 };
