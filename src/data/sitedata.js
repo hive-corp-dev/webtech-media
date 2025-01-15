b61a065ef3f4d7d452a3a8e6c8ab6bdb69f17d86
Tatsuya <a.tatsu0117@gmail.com>
2025-01-15 21:52:37 +0900

    added article and footer links

--- src/data/sitedata.js
+++ src/data/sitedata.js
@@ -1,1 +1,3 @@
 export const MAIL_ADDRESS = "info@webtech-media.jp";
+export const X_PROFILE_URL = "https://x.com/tatsuya_webdev";
+export const GITHUB_PROFILE_URL = "https://github.com/Tatsuya-akao";
