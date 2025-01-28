c793fe32742e0bbec3d4f82b9b23e72400c6906a
Tatsuya <a.tatsu0117@gmail.com>
2025-01-28 23:09:10 +0900

    fixed demo

--- src/data/article-ranking.js
+++ src/data/article-ranking.js
@@ -1,8 +1,8 @@
 export const rankingPostIds = [
-  "astro-background-image-optimization.mdx",
-  "nextjs-14-app-router-microsoft-clarity.mdx",
   "astro-sass-settings-and-usage.mdx",
+  "astro-background-image-optimization.mdx",
+  "astro-partytown-google-analytics.mdx",
+  "nextjs-14-app-router-microsoft-clarity.mdx",
   "astro-get-url.mdx",
   "nextjs-14-app-router-google-analytics.mdx",
-  "astro-partytown-google-analytics.mdx",
 ];
