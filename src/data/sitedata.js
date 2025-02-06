1eeb364cadeedb7ab3d3fd767a28f24c1a56aa93
Tatsuya <a.tatsu0117@gmail.com>
2025-02-06 23:25:06 +0900

    added pagination

--- src/data/sitedata.js
+++ src/data/sitedata.js
@@ -1,3 +1,5 @@
 export const MAIL_ADDRESS = "info@webtech-media.jp";
 export const X_PROFILE_URL = "https://x.com/tatsuya_webdev";
 export const GITHUB_PROFILE_URL = "https://github.com/Tatsuya-akao";
+
+export const POSTS_PER_PAGE = 10;
