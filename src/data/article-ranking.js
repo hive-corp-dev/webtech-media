1e02ae5c27cd9e99f6e87b0115672b72475b5460
Tatsuya <a.tatsu0117@gmail.com>
2025-02-16 00:00:31 +0900

    changed ranking

--- src/data/article-ranking.js
+++ src/data/article-ranking.js
@@ -1,8 +1,8 @@
 export const rankingPostIds = [
   "astro-sass-settings-and-usage.mdx",
   "astro-background-image-optimization.mdx",
-  "astro-partytown-google-analytics.mdx",
-  "nextjs-14-app-router-microsoft-clarity.mdx",
   "astro-get-url.mdx",
+  "astro-partytown-google-analytics.mdx",
   "nextjs-14-app-router-google-analytics.mdx",
+  "nextjs-14-app-router-microsoft-clarity.mdx",
 ];
